d34667c6d4785a77c7cbdbc570edd8451bf09979
Piotr Krawiecki <pkrawiecki@gmail.com>
2017-04-09 22:46:46 +0200

    Add project files.

---
+++ ecs-dotnet/CommandLineOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace ecs_dotnet
+{
+    class CommandLineOptions
+    {
+        [Option(shortName: 's', longName: "section", HelpText = "Name of Section to encrypt", Required = true)]
+        public string Section { get; set; }
+
+        [Option(shortName: 'g', longName: "group", HelpText = "SectionGroup name where Section is placed in", Required = false)]
+        public string SectionGroup { get; set; }
+
+        [Option(shortName: 'c', longName: "config", HelpText = "Path to .config file", Required = true)]
+        public string ConfigPath { get; set; }
+    }
+}
---
+++ ecs-dotnet/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ecs_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            ClientSettingsSection section = null;
+            ConfigurationSectionGroup group = null;
+            var options = new CommandLineOptions();
+            var isValid = CommandLine.Parser.Default.ParseArgumentsStrict(args, options);
+
+            sb.Append(string.Format("Trying to encrypt Section {0} ", options.Section));
+            if(string.IsNullOrEmpty(options.SectionGroup) == false)
+            {
+                sb.Append(string.Format("under {0} ", options.SectionGroup));
+            }
+            sb.Append(string.Format("in file {0}", options.ConfigPath));
+
+            Console.WriteLine(sb.ToString());
+
+
+            if( System.IO.File.Exists(options.ConfigPath) == false)
+            {
+                Console.WriteLine("File {0} does not exists!", options.ConfigPath);
+                return;
+            }
+
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+            fileMap.ExeConfigFilename = options.ConfigPath;
+
+            // Get the current configuration file.
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+
+            if (string.IsNullOrEmpty(options.SectionGroup) == false)
+            {
+                group = config.SectionGroups[options.SectionGroup];
+                if (group == null)
+                {
+                    Console.WriteLine("Could not read SectionGroup {0}", options.SectionGroup);
+                    return;
+                }
+
+                section = group.Sections[options.Section] as ClientSettingsSection;
+                if (section == null)
+                {
+                    Console.WriteLine("Could not read Section {0}", options.Section);
+                    return;
+                }
+            }
+            else
+            {
+                section = config.Sections[options.Section] as ClientSettingsSection;
+                if (section == null)
+                {
+                    Console.WriteLine("Could not read Section {0}", options.Section);
+                    return;
+                }
+            }
+            
+            section.SectionInformation.ProtectSection("RsaProtectedConfigurationProvider");
+
+            section.SectionInformation.ForceSave = true;
+            config.Save(ConfigurationSaveMode.Full);
+
+            Console.WriteLine("Section {0} encrypted successfully!", options.Section);
+        }
+    }
+}
